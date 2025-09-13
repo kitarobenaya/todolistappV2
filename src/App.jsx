48cb341e6a2272f533577b340b3fd67755b3f760
kitarobenaya <kitarobenaya09@gmail.com>
2025-09-13 17:42:55 +0700

    commit ke 1 memulai project pertama dengan react

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+
+export default function App() {
+    return (
+      <>
+        <header>
+          <h1 className="text-4xl font-bold text-center font-[Montserrat] mt-4 text-[#424242] animate-title underline underline-offset-8">
+            Schedule Your Day with Kitaro's To-Do List App
+          </h1>
+        </header>
+
+        <main className='size-full bg-red-500'>
+          
+        </main>
+      </>      
+    )
+}
+
+
